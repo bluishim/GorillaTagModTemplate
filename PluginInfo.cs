34905e1482f9811f96c59b99ab9bfd849a75d768
daboo <daboo@NASAPC>
2024-04-05 22:44:21 -0600

    Add project files.

---
+++ PluginInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace GorillaTagModTemplate
+{
+    // This is your GUID, which shows in Unity Logs and illegal mod checkers.
+    // Feel free to change the Name and GUID!
+    class PluginInfo
+    {
+        public const string GUID = "com.AuthorLowerName.gorillatag.GorillaTagLowerModTemplateProject";
+        public const string Name = "GorillaTagModTemplateProject";
+        public const string Version = "1.0.0";
+    }
+}
